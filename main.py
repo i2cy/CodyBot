41b791342dac1eb4c804563717b4013af904c8b8
icy <i2cy@outlook.com>
2021-03-10 00:17:51 +0000

    added main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# Author: i2cy(i2cy@outlook.com)
+# Filename: main
+# Created on: 2021/3/9
+
+import i2cylib.database.sqlite as sqlib
+from i2cylib.utils.logger import *
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
